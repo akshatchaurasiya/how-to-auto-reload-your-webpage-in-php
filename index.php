780fb8a817063975b744bfae873463bb2239cee5
Akshat Chaurasiya <102651258+akshatchaurasiya@users.noreply.github.com>
2022-05-06 12:36:46 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// reload code...
+echo("<meta http-equiv='refresh' content='60'>");
+// end reload code..
+
+// your web page code.....
+echo date('H:i:s Y-m-d');
+// write something.......
+// your code on here
+?>
